9302ba128d8a136cab44ee93ff0abd8c060625c7
Sergei_Isaichenko <128219235+Sergo616@users.noreply.github.com>
2023-08-28 02:55:16 -0700

    Add files via upload

---
+++ Simple animation[Fireplace]/main.js
@@ -0,0 +1,12 @@
+﻿// Created by Runtime Terror 
+
+// Created by Sergei Isáichenko
+
+const singlePlay = selectedId => {
+    
+     // make sure all music are paused first
+    document.querySelectorAll("audio")
+      .forEach(aud => aud.pause());
+    // play the chosen one
+     document.getElementById(selectedId).play();
+};
